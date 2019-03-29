590ff5578812d830a1dbff89044b0540386f1331
agonzalez33 <44071440+agonzalez33@users.noreply.github.com>
2019-03-29 09:19:41 -0400

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,19 @@
+from turtle import*
+import random
+
+
+skye = Turtle()
+skye.color("red")
+skye.pensize(3)
+skye.shape("turtle")
+skye.speed(0)
+
+
+for x in range(3):
+		skye.forward(60)
+		skye.left(120)
+
+mainloop()	
+
+
+					
